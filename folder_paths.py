97c0cd08e25a9985d975314b9a08a178533994c5
Yassine Belatar <76491500+yassinebelatar@users.noreply.github.com>
2024-10-19 09:36:16 +0100

    Update folder_paths.py
    YOLOv8 PATH

--- folder_paths.py
+++ folder_paths.py
@@ -38,7 +38,7 @@ folder_names_and_paths["hypernetworks"] = ([os.path.join(models_dir, "hypernetwo
 folder_names_and_paths["photomaker"] = ([os.path.join(models_dir, "photomaker")], supported_pt_extensions)
 
 folder_names_and_paths["classifiers"] = ([os.path.join(models_dir, "classifiers")], {""})
-
+folder_names_and_paths["yolov8"] = ([os.path.join(models_dir, "yolov8")], supported_pt_extensions)
 output_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "output")
 temp_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "temp")
 input_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "input")
